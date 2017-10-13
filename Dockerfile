d67ffb51547e8ca54ce83741fd6bdad08775f4a7
Rosemary Wang <rosewang@thoughtworks.com>
2017-10-13 08:39:46 -0500

    With updated handler, improved outputs that uses config.json

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM consul:latest
+
 RUN mkdir /scripts
+
 COPY handler /scripts/
+
+COPY config.json /consul/config/
+
 RUN chmod +x /scripts/handler
--- handler.go
+++ handler.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"os"
 
@@ -11,36 +10,24 @@ import (
 )
 
 func main() {
-	filename := flag.String("filename", "/scripts/consul_watch.log", "file path")
-	ansible_runner := flag.String("runner", "127.0.0.1", "URL of Ansible Playbook Runner")
-
-	flag.Parse()
-
-	f, err := os.OpenFile(*filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
+	ansible_runner, ok := os.LookupEnv("CONSUL_HANDLER_RUNNER")
+	if !ok {
+		panic("Environment variable CONSUL_HANDER_RUNNER is not set!")
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
 
-	runner_client := runner.Client{URL: *ansible_runner}
+	runner_client := runner.Client{URL: ansible_runner}
 
 	for scanner.Scan() {
 		input := scanner.Bytes()
-		if len(input) > 1 {
-			network, err := lib.GetNewestNetwork(input)
+		network, err := lib.GetNewestNetwork(input)
+		if len(network) > 0 {
 			if err != nil {
 				panic(err)
 			}
-
-			if _, err = f.WriteString(network + "\n"); err != nil {
-				panic(err)
-			}
-			string_play_body := fmt.Sprintf("{\"container_network\": \"%s\"}", network)
-			fmt.Println(string_play_body)
-			play_body := []byte(string_play_body)
-
+			play_body := []byte(fmt.Sprintf("{\"container_network\": \"%s\"}", network))
 			if _, err = runner_client.Play(play_body); err != nil {
 				fmt.Printf("Network %s not created", network)
 			}
